cbf21ec5ad4526c2590c27244bf51b705ea99b05
Hamza Sheikh <hsheikhm@gmail.com>
2015-10-28 12:31:46 +0000

    implemented test helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario "submitting names" do
-      visit('/')
-      fill_in('Player 1 Name', with: 'Hamza')
-      fill_in('Player 2 Name', with: 'Eleni')
-      click_button('Submit')
-      expect(page).to have_content('Hamza vs. Eleni')
+    sign_in_and_play
+    expect(page).to have_content('Hamza vs. Eleni')
   end
 end
