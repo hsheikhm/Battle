4dc4bf44f452e27df0781a226893b85c9a34f78f
Hamza Sheikh <hsheikhm@gmail.com>
2015-10-28 18:45:23 +0000

    improved button

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,7 @@
 feature 'attack' do
   scenario 'attacks player 2' do
     sign_in_and_play
-    click_button('attack')
+    click_button('Attack')
     expect(page).to have_content('Hamza attacked Eleni')
   end
 end
